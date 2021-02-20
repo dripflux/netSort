e66015d90513bb9e25ad72db17137c2c7a64e362
Drip.Flux <af86dk95yf@liamekaens.com>
2021-02-20 17:35:11 -0500

    Updated source material (netSort.py: changed 'count' interface to 'sort',   changed 'FORMAT_IN' naming to 'IN_FORMAT',   add 'OUT_DATA' features, add 'OUT_FORMAT' features,   initial implement ProcPackets.processPerMode(),   add + initial implement ProcPackets.__processGroupBy(),   add + initial implement outputResults()).

--- netSort.py
+++ netSort.py
@@ -6,14 +6,14 @@ NAME
 
 SYNOPSIS
 	netSort metadataFile...
-	netSort [group <src | dest | connect | proto>] [count <packets | bytes>] [order <low | high>] metadataFile...
+	netSort [group <src | dest | connect | proto>] [sort <packets | bytes>] [order <low | high>] metadataFile...
 	netSort help
 
 DESCRIPTION
 	Process network traffic packet metadata from metadataFile.
 	Default I/O format is CSV discussed in RawPacket (ID, relative time, source address, destination address, protocol, payload bytes).
 	Group packets by 'group' per below.
-	Count packet groups by 'count' per below.
+	Sort packet groups by 'sort' per below.
 	Order output based on 'order' per below.
 
 	group
@@ -22,9 +22,9 @@ DESCRIPTION
 		connect : Group packets by source and destination pairing permutations, a -> b is separate from b -> a.
 		proto : Group packets by protocol.
 
-	count
-		packets : (default) Count number of packets for group.
-		bytes : Count total bytes sent for group.
+	sort
+		packets : (default) Sort by number of packets for group.
+		bytes : Sort by total bytes sent for group.
 
 	order
 		low : (default) Order output numerical low to high (i.e. normal sorting).
@@ -39,35 +39,57 @@ import sys  # System Module: argv
 # Declare Required Constants (Immutables)
 # Mode Bits
 # Group By
-# 0|000| 000|0 00|00 1|111
+# 1|111
 GROUP_BY_MASK        = 0o17  # Bits 0-3
 GROUP_BY_USE_DEFAULT = 0o00
 GROUP_BY_SRC_ADDR    = 0o01
 GROUP_BY_DEST_ADDR   = 0o02
 GROUP_BY_CONNECT     = 0o03
 GROUP_BY_PROTO       = 0o04
+GROUP_BY_EXTEND_01   = 0o17
 GROUP_BY_DEFAULT     = GROUP_BY_SRC_ADDR
 # Count
-# 0|000| 000|0 11|11 0|000
-COUNT_MASK        = 0o360  # Bits 4-7
-COUNT_USE_DEFAULT = 0o000
-COUNT_PACKETS     = 0o020
-COUNT_BYTES       = 0o040
-COUNT_DEFAULT     = COUNT_PACKETS
+# 11|11 0|000
+SORT_MASK        = 0o360  # Bits 4-7
+SORT_USE_DEFAULT = 0o000
+SORT_PACKETS     = 0o020
+SORT_BYTES       = 0o040
+SORT_EXTEND_01   = 0o360
+SORT_DEFAULT     = SORT_PACKETS
 # Order
-# 0|000| 111|1 00|00 0|000
+# 111|1 00|00 0|000
 ORDER_MASK        = 0o7400  # Bits 8-11
 ORDER_USE_DEFAULT = 0o0000
 ORDER_NUM_LOW     = 0o0400
 ORDER_NUM_HIGH    = 0o1000
+ORDER_EXTEND_01   = 0o7400
 ORDER_DEFAULT     = ORDER_NUM_LOW
 # Input Format
 # 1|111| 000|0 00|00 0|000
-FORMAT_IN_MASK          = 0o170000  # Bits 12-15
-FORMAT_IN_USE_DEFAULT   = 0o000000
-FORMAT_IN_CSV_HEADER    = 0o010000
-FORMAT_IN_CSV_NO_HEADER = 0o020000
-FORMAT_IN_DEFAULT       = FORMAT_IN_CSV_HEADER
+IN_FORMAT_MASK          = 0o170000  # Bits 12-15
+IN_FORMAT_USE_DEFAULT   = 0o000000
+IN_FORMAT_CSV_HEADER    = 0o010000
+IN_FORMAT_CSV_NO_HEADER = 0o020000
+IN_FORMAT_EXTEND_01     = 0o170000
+IN_FORMAT_DEFAULT       = IN_FORMAT_CSV_HEADER
+# Output Data
+# 11|11 0|000| 000|0 00|00 0|000
+OUT_DATA_MASK        = 0o3600000  # Bits 16-19
+OUT_DATA_USE_DEFAULT = 0o0000000
+OUT_DATA_TRACK_SORT  = 0o0200000
+OUT_DATA_PACKETS     = 0o0400000
+OUT_DATA_BYTES       = 0o1000000
+OUT_DATA_EXTEND_01   = 0o2000000
+OUT_DATA_DEFAULT     = OUT_DATA_TRACK_SORT
+# Output Format
+# 111|1 00|00 0|000| 000|0 00|00 0|000
+OUT_FORMAT_MASK        = 0o74000000  # Bits 20-23
+OUT_FORMAT_USE_DEFAULT = 0o00000000
+OUT_FORMAT_TSV_HUMAN   = 0o04000000
+OUT_FORMAT_TSV_SIMPLE  = 0o10000000
+OUT_FORMAT_CSV         = 0o14000000
+OUT_FORMAT_EXTEND_01   = 0o74000000
+OUT_FORMAT_DEFAULT     = OUT_FORMAT_TSV_HUMAN
 
 # Declare Required Variables (Mutables)
 config = {}
@@ -157,7 +179,7 @@ class ProcPacket :
 		self.count = 0
 		self.bytes = 0
 		if packet is not None :
-			modeGroup = config[mode] & GROUP_BY_MASK
+			modeGroup = config["mode"] & GROUP_BY_MASK
 			if modeGroup == GROUP_BY_USE_DEFAULT :
 				modeGroup = GROUP_BY_DEFAULT
 			if modeGroup == GROUP_BY_SRC_ADDR :
@@ -184,29 +206,29 @@ class ProcPacket :
 			self
 			, other
 		) :
-		modeCount = config[mode] & COUNT_MASK
-		if modeCount == COUNT_USE_DEFAULT :
-			modeCount = COUNT_DEFAULT
-		if modeCount == COUNT_PACKETS :
+		modeSort = config["mode"] & SORT_MASK
+		if modeSort == SORT_USE_DEFAULT :
+			modeSort = SORT_DEFAULT
+		if modeSort == SORT_PACKETS :
 			return (self.count == other.count) and (self.group == other.group)
-		elif modeCount == COUNT_BYTES :
+		elif modeSort == SORT_BYTES :
 			return (self.bytes == other.bytes) and (self.group == other.group)
 
 	def __ge__(
 			self
 			, other
 		) :
-		modeCount = config[mode] & COUNT_MASK
-		if modeCount == COUNT_USE_DEFAULT :
-			modeCount = COUNT_DEFAULT
-		if modeCount == COUNT_PACKETS :
+		modeSort = config["mode"] & SORT_MASK
+		if modeSort == SORT_USE_DEFAULT :
+			modeSort = SORT_DEFAULT
+		if modeSort == SORT_PACKETS :
 			if self.count > other.count :
 				return True
 			elif self.count == other.count :
 				return self.group >= other.group
 			else :
 				return False
-		elif modeCount == COUNT_BYTES :
+		elif modeSort == SORT_BYTES :
 			if self.bytes > other.bytes :
 				return True
 			elif self.bytes == other.bytes :
@@ -218,17 +240,17 @@ class ProcPacket :
 			self
 			, other
 		) :
-		modeCount = config[mode] & COUNT_MASK
-		if modeCount == COUNT_USE_DEFAULT :
-			modeCount = COUNT_DEFAULT
-		if modeCount == COUNT_PACKETS :
+		modeSort = config["mode"] & SORT_MASK
+		if modeSort == SORT_USE_DEFAULT :
+			modeSort = SORT_DEFAULT
+		if modeSort == SORT_PACKETS :
 			if self.count > other.count :
 				return True
 			elif self.count == other.count :
 				return self.group > other.group
 			else :
 				return False
-		elif modeCount == COUNT_BYTES :
+		elif modeSort == SORT_BYTES :
 			if self.bytes > other.bytes :
 				return True
 			elif self.bytes == other.bytes :
@@ -240,17 +262,17 @@ class ProcPacket :
 			self
 			, other
 		) :
-		modeCount = config[mode] & COUNT_MASK
-		if modeCount == COUNT_USE_DEFAULT :
-			modeCount = COUNT_DEFAULT
-		if modeCount == COUNT_PACKETS :
+		modeSort = config["mode"] & SORT_MASK
+		if modeSort == SORT_USE_DEFAULT :
+			modeSort = SORT_DEFAULT
+		if modeSort == SORT_PACKETS :
 			if self.count < other.count :
 				return True
 			elif self.count == other.count :
 				return self.group <= other.group
 			else :
 				return False
-		elif modeCount == COUNT_BYTES :
+		elif modeSort == SORT_BYTES :
 			if self.bytes < other.bytes :
 				return True
 			elif self.bytes == other.bytes :
@@ -262,17 +284,17 @@ class ProcPacket :
 			self
 			, other
 		) :
-		modeCount = config[mode] & COUNT_MASK
-		if modeCount == COUNT_USE_DEFAULT :
-			modeCount = COUNT_DEFAULT
-		if modeCount == COUNT_PACKETS :
+		modeSort = config["mode"] & SORT_MASK
+		if modeSort == SORT_USE_DEFAULT :
+			modeSort = SORT_DEFAULT
+		if modeSort == SORT_PACKETS :
 			if self.count < other.count :
 				return True
 			elif self.count == other.count :
 				return self.group < other.group
 			else :
 				return False
-		elif modeCount == COUNT_BYTES :
+		elif modeSort == SORT_BYTES :
 			if self.bytes < other.bytes :
 				return True
 			elif self.bytes == other.bytes :
@@ -294,7 +316,7 @@ class ProcPackets :
 	def __init__(
 			self
 			, file = None
-			, format = FORMAT_IN_USE_DEFAULT
+			, format = IN_FORMAT_USE_DEFAULT
 		) :
 		"""
 		Description: Initialize an empty packet container, or with specified data from file per format.
@@ -309,13 +331,13 @@ class ProcPackets :
 	def appendPackets(
 			self
 			, file = None
-			, format = FORMAT_IN_USE_DEFAULT
+			, format = IN_FORMAT_USE_DEFAULT
 		) :
 		if file is not None :
 			# Prepare for opening input file
-			formatIn = format & FORMAT_IN_MASK
-			if formatIn == FORMAT_IN_USE_DEFAULT :
-				formatIn = FORMAT_IN_DEFAULT
+			formatIn = format & IN_FORMAT_MASK
+			if formatIn == IN_FORMAT_USE_DEFAULT :
+				formatIn = IN_FORMAT_DEFAULT
 			fileOpenMode = "rt"
 			try :
 				packetsFile = open(file, mode=fileOpenMode)
@@ -323,10 +345,10 @@ class ProcPackets :
 				raise
 			# Convert input file to packet per line format
 			packetPerLine = []
-			if (formatIn & FORMAT_IN_CSV_HEADER) or (formatIn & FORMAT_IN_CSV_NOHEADER) :
+			if (formatIn & IN_FORMAT_CSV_HEADER) or (formatIn & IN_FORMAT_CSV_NOHEADER) :
 				packetPerLine = packetsFile
 			skipFirst = False
-			if formatIn & FORMAT_IN_CSV_HEADER :
+			if formatIn & IN_FORMAT_CSV_HEADER :
 				skipFirst = True
 			# Process packet per line
 			for packetLine in packetPerLine :
@@ -347,10 +369,31 @@ class ProcPackets :
 		Arguments:
 			mode : Mode to group, count, and order RawPackets per.
 		Returns:
-			[list] : List of tuples per group, count, and order processed packet data.
+			[list] : List of ProcPacket objects grouped and ordered, each ProcPacket has count and bytes data.
 		"""
-		...
+		if mode is not None :
+			combinedMasks = GROUP_BY_MASK | SORT_MASK | ORDER_MASK
+			config["mode"] = (combinedMasks & mode) | (config["mode"] & ~combinedMasks)
+		self.__processGroupBy()
+		self.__resultPackets = list(self.__procPackets.values())
+		self.__resultPackets.sort()
 		return self.__resultPackets.copy()
+
+	def __processGroupBy(
+			self
+		) :
+		"""
+		Description: Process RawPackets based on group mode.
+		"""
+		# Set up for processing
+		self.__procPackets.clear()
+		# Traverse and group raw packets
+		for rawPacket in self.__rawPackets :
+			procPacket = ProcPacket(rawPacket)
+			if procPacket.group not in self.__procPackets :
+				self.__procPackets[procPacket.group] = procPacket
+			else :
+				self.__procPackets[procPacket.group] += procPacket
 
 	def connectionByBytes(
 			self
@@ -459,8 +502,8 @@ class ProcPackets :
 		else :
 			procMode = config["mode"] & ORDER_MASK
 		procMode = procMode & ~GROUP_BY_MASK
-		procMode = procMode & ~COUNT_MASK
-		procMode = procMode | GROUP_BY_SOURCE | COUNT_PACKETS
+		procMode = procMode & ~SORT_MASK
+		procMode = procMode | GROUP_BY_SRC_ADDR | SORT_PACKETS
 		return self.processPerMode(procMode)
 
 	def clear(
@@ -522,10 +565,34 @@ def configureDefaults(
 	"""
 	config["mode"] = \
 	    GROUP_BY_USE_DEFAULT \
-	  | COUNT_USE_DEFAULT \
+	  | SORT_USE_DEFAULT \
 	  | ORDER_USE_DEFAULT \
-	  | FORMAT_IN_USE_DEFAULT
-	...
+	  | IN_FORMAT_USE_DEFAULT \
+	  | OUT_DATA_USE_DEFAULT \
+	  | OUT_FORMAT_USE_DEFAULT
 
+def outputResults(
+		results
+		, file = sys.stdout
+		, mode = None
+	) :
+	"""
+	Description: ...
+	"""
+	if mode is not None :
+		outMode = mode & SORT_MASK
+	else :
+		outMode = config["mode"] & SORT_MASK
+	if outMode == SORT_USE_DEFAULT :
+		outMode = SORT_DEFAULT
+	for resultProcPacket in results :
+		outData = str(resultProcPacket.group) + "\t"
+		if outMode == SORT_PACKETS :
+			outData += str(resultProcPacket.count)
+		elif outMode == SORT_BYTES :
+			outData += str(resultProcPacket.bytes)
+		print(outData, file=file)
+
+configureDefaults()
 if __name__ == "__main__" :  # Called as standalone program
 	main()
